ca942aeea0cf61b90d3fa16c84286c1903883177
goldBatzmaru <36875823+goldbatzmaru@users.noreply.github.com>
2018-03-07 17:00:53 -0800

    Update custom.js
    Updated calendar construction function

--- kame-forma/assets/js/custom.js
+++ kame-forma/assets/js/custom.js
@@ -183,22 +183,67 @@
 		});
 	}
 
+	function getDateClasses(date){
+        date = new Date(date);
+        var month = 'month-' + date.getMonth() + 1;
+        var day = 'day-' + date.getDate();
+        var year = 'year-' + date.getFullYear();
+
+        return month + ' ' + day + ' ' + year;
+	}
+
+	function getMonthName(date){
+
+        date = new Date(date);
+        var month = [];
+        month[0] = "January";
+        month[1] = "February";
+        month[2] = "March";
+        month[3] = "April";
+        month[4] = "May";
+        month[5] = "June";
+        month[6] = "July";
+        month[7] = "August";
+        month[8] = "September";
+        month[9] = "October";
+        month[10] = "November";
+        month[11] = "December";
+        return month[date.getMonth()];
+
+	}
+
     function createEventList(events){
 
 		var eventList = '<ul id="event-list">';
         $.each(events['event-list'],function(index, value){
-        	var event = '<li class="event col-sm-6 col-xs-12"><div class = "event-inner-wrapper">';
-        	if(value['title'] != null){
-        		var eventTitle = '<div class="event-title">'+value['title']+'</div>';
-				event += eventTitle;
+        	var dateClasses = null;
+        	var location, time;
+        	var locationCheck = value.location != null;
+        	var timeCheck = value.time != null;
+            if(locationCheck) {
+                location = value.location[0];
+            }
+
+            if(timeCheck) {
+                time = value.time[0];
+                dateClasses = getDateClasses(time.month+'-'+time.day+'-'+time.year);
+            }
+
+        	var event = '<li class="event col-sm-6 col-xs-12 ' + dateClasses +'"><div class = "event-inner-wrapper">';
+            if(timeCheck){
+            	var monthName = getMonthName(time.month+'-'+time.day+'-'+time.year);
+                event += '<div class="event-date">'+monthName+' '+time.day+', '+time.year+'</div>';
+            }
+        	if(value.title != null){
+				event += '<div class="event-title">'+value.title+'</div>';
         	}
 
-        	if(value['acts'] != null){
+        	if(value.acts != null){
         		var acts = '<div class="acts">';
         		var actCount = 0;
-        		var actAmount = value['acts'].length;
-    			$.each(value['acts'], function(x, y){
-					var act = '<a href="'+y['url_1']+'" class="act-title">'+y['title']+'</a>';
+        		var actAmount = value.acts.length;
+    			$.each(value.acts, function(x, y){
+					var act = '<a href="'+y.url_1+'" class="act-title">'+y.title+'</a>';
 					actCount++;
 					if(actCount != actAmount){
 						act += ', ';
@@ -210,41 +255,36 @@
     			event += acts;
         	}
 
-        	if(value['location'] != null){
-        		var location = value['location'][0];
-				var eventLocation = '<div class="event-location"><div class="location-name">'+location['name']+'</div><div class="event-address">';
-        		var addressGiven = (location['address_given'] == 'true');
+        	if(locationCheck){
+				var eventLocation = '<div class="event-location"><div class="location-name">'+location.name+'</div><div class="event-address">';
+        		var addressGiven = (location.address_given == 'true');
 
         		if((addressGiven == false) || (addressGiven == null)){
-        			eventLocation += '<div class="address-hint">'+location['address_hint']+'</div>';
+        			eventLocation += '<div class="address-hint">'+location.address_hint+'</div>';
         		} else {
-        			eventLocation += '<div class="street">'+location['street']+'</div>';
-        			eventLocation += '<div class="city-state">'+location['city']+', '+location['state']+'</div>';
-        			eventLocation += '<div class="zip">'+location['zipcode']+'</div>';
+        			eventLocation += '<div class="street">'+location.street+'</div>';
+        			eventLocation += '<div class="city-state">'+location.city+', '+location.state+'</div>';
+        			eventLocation += '<div class="zip">'+location.zipcode+'</div>';
         		}
         		eventLocation += '</div></div>';
     			event += eventLocation;
         	}
 
-        	if(value['time'] != null){
-        		var time = value['time'][0];
+        	if(timeCheck){
 				var schedule = '<div class="time">';
-				schedule += '<div class="date">'+time['day']+'/'+time['month']+'/'+time['year']+'</div>';
-				schedule += '<div class="doors">Doors at '+time['doors']+'</div>';
-				schedule += '<div class="duration">'+ time['music_start'] + ' - ' + time['music_end'] + '</div></div>';
+				// schedule += '<div class="date">'+time['day']+'/'+time['month']+'/'+time['year']+'</div>';
+				schedule += '<div class="doors">Doors at '+time.doors+'</div>';
+				schedule += '<div class="duration">'+ time.music_start + ' - ' + time.music_end + '</div></div>';
 				event += schedule;
         	}
-        	if(value['cover'] != null){
-    			var cover = '<div class="cover">'+value['cover']+'</div>';
-    			event += cover;
+        	if(value.cover != null){
+    			event += '<div class="cover">'+value.cover+'</div>';
         	}
-        	if(value['age_policy'] != null){
-        		var policy = '<div class="age-policy">'+value['age_policy']+'</div>';
-        		event += policy;
+        	if(value.age_policy != null){
+        		event += '<div class="age-policy">'+value.age_policy+'</div>';
         	}
-        	if(value['additional_info'] != null){
-        		var add_info = '<div class="add-info">'+value['additional_info']+'</div>';
-        		event += add_info;
+        	if(value.additional_info != null){
+        		event += '<div class="add-info">'+value.additional_info+'</div>';
         	}
         	event += '</div></li>';
 			eventList += event;
